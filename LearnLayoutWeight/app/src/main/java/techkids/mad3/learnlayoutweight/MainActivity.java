2f455dde8b83185c41df974f3ff313078a9e0b3c
Trần Trung <nguyentrantrung1986@gmail.com>
2016-06-29 03:52:49 +0700

    set layout_weight to 0

---
+++ LearnLayoutWeight/app/src/main/java/techkids/mad3/learnlayoutweight/MainActivity.java
@@ -0,0 +1,13 @@
+package techkids.mad3.learnlayoutweight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
